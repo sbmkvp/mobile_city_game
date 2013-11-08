1d18a4b24b27cba7a9a9c81559ad9f3c5ad3ed1e
sbmkvp <sbm.kvp@gmail.com>
2013-11-08 23:29:05 +0000

    some comments on the code.

--- Game/map.js
+++ Game/map.js
@@ -39,22 +39,25 @@ function drawMap () {
 //This function gets the data from the data.js file and creates a marker for each one of them.
 function markStations() {
 	var data = getData();
-	var image = new google.maps.MarkerImage('mr1.png', new google.maps.Size(10,10), new google.maps.Point(0,0), new google.maps.Point(5,5));
 	var bounds = new google.maps.LatLngBounds();
 	for (i in data.stations) {
 		var loc = new google.maps.LatLng(data.stations[i].lat,data.stations[i].lon);
+
+		//this part defines how the markers are to be shown basically the size and look of the markers.
 		marker = new google.maps.Marker({
 			position: loc,
 			title:data.stations[i].name,
 			map: map,
 			draggable: false,
-			icon: image,
+			icon: new google.maps.MarkerImage('mr1.png', new google.maps.Size(10,10), new google.maps.Point(0,0), new google.maps.Point(5,5)),
 		});
 		
+		// this part of the code tells the map to produce the infoWindow when the marker is clicked.
 		google.maps.event.addListener(marker,'click', function(){
 			infoWindow.setContent(this.title);
 			infoWindow.open(map, this);
 		});
+		
 		stations.push(marker);
 		bounds.extend(loc);
 	}
